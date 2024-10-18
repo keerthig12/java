3bfda5081cc955a2957ef4d128973c809842d1a8
keerthig12 <keerthimg02@gmail.com>
2024-10-18 12:49:05 +0530

    methods file

---
+++ methods.java
@@ -0,0 +1,23 @@
+public class methods {
+ 
+  public static void main (String[] args){
+
+    goodMorning();
+    goodMorning();
+    goodMorning();
+
+      country();
+       country();
+        country();
+
+ }
+
+       public static void goodMorning(){
+           System.out.println("say good morning to me");
+ 
+       }
+
+       public static void country(){
+           System.out.println("India is a beautiful country");
+       }
+}
