13f384dfb06df048882f32119bcb7ddb46848b76
keerthig12 <keerthimg02@gmail.com>
2024-10-18 12:11:16 +0530

    types

---
+++ Datatype.java
@@ -0,0 +1,161 @@
+public class Datatype{
+
+	public static void main(String[] args){
+
+byte age = 103;
+byte numberOfAlphabets = 26;
+byte daysInAMonth = 31;
+byte numberOfClassInASchool = 13;
+byte percentage = 87;
+byte numberOfChildrenInAHouse = 5;
+byte numberOfVehicle = 6;
+byte phValue = 4;
+byte roomTempature = 37;
+byte numberOfPhonesInAHouse = 8;
+ 
+ System.out.println(age);
+ System.out.println(numberOfAlphabets);
+ System.out.println(daysInAMonth);
+ System.out.println(numberOfClassInASchool);
+ System.out.println(percentage);
+System.out.println(numberOfChildrenInAHouse);
+System.out.println(numberOfVehicle);
+System.out.println(phValue);
+System.out.println(roomTempature);
+System.out.println(numberOfPhonesInAHouse);
+
+
+short riversInIndia = 400;
+short countriesInWorld = 195;
+short damsInIndia = 5781;
+short banksInIndia = 2500;
+short overAllRunsOfSachin = 18486;
+short daysInAYear = 365;
+short numberOfStudentsInCOllege = 5000; 
+short globalUniversities = 27000;
+short numberOfDistrictsInIndia = 800;
+short stateUniversitiesInIndia =  479;
+
+System.out.println(riversInIndia);
+System.out.println(countriesInWorld);
+System.out.println(damsInIndia);
+System.out.println(banksInIndia);
+System.out.println(overAllRunsOfSachin);
+System.out.println(numberOfStudentsInCOllege);
+System.out.println(globalUniversities);
+System.out.println(numberOfDistrictsInIndia);
+System.out.println(stateUniversitiesInIndia);
+
+int priceOfBike = 180000;
+int populationOfAState = 19000000;
+int numberOfBooksInLibrary = 400000;
+int mobilePrice = 55000;
+int laptopPrice  = 200000;
+int carPrice = 30000000;
+int productQuantities = 100000;
+int postViews = 976543210;
+int governmentBudget = 987654329;
+int productSales = 250000;
+
+System.out.println(priceOfBike);
+System.out.println(populationOfAState);
+System.out.println(numberOfBooksInLibrary);
+System.out.println(mobilePrice);
+System.out.println(laptopPrice);
+System.out.println(carPrice);
+System.out.println(productQuantities);
+System.out.println(postViews);
+System.out.println(governmentBudget);
+System.out.println(productSales);
+
+long populationCount = 8543212345l;
+long transactionID = 9876543210l;
+long accountBalance = 960949658901L;
+long astronomicalDistances = 9461000000000L;
+long likeCountsOnAPost = 4782382364889l;
+long commentOnAPost = 987654321098765l;
+long salesTransactionID = 9876543210987l;
+long creditCardNumbers = 543210987654321l;
+long insurancePolicyNumbers = 123456789012345l;
+
+System.out.println(populationCount);
+System.out.println(transactionID);
+System.out.println(accountBalance);
+System.out.println(astronomicalDistances);
+System.out.println(likeCountsOnAPost);
+System.out.println(commentOnAPost);
+System.out.println(salesTransactionID);
+System.out.println(creditCardNumbers);
+System.out.println(insurancePolicyNumbers);
+
+float smallDecimalValues = 0.0456f;
+float scientificCalculations = 1.234567810f;
+float stockPrice = 123.45f;
+float javaFloatValue = 3.14159f;
+float oneUsd = 0.8765f;
+float animationKeyFrameValues = 1.2345f;
+float distanceCalculations = 12.345f;
+float musicProcessingAudioSignalValues = 0.1234f;
+float imageRecognitionPixelValues = 123.45f;
+float recommendationSystemsUserRatings = 4.255f;
+
+System.out.println(smallDecimalValues);
+System.out.println(scientificCalculations);
+System.out.println(stockPrice);
+System.out.println(javaFloatValue);
+System.out.println(oneUsd);
+System.out.println(animationKeyFrameValues);
+System.out.println(distanceCalculations);
+System.out.println(musicProcessingAudioSignalValues);
+System.out.println(imageRecognitionPixelValues);
+System.out.println(recommendationSystemsUserRatings);
+
+double bankBalance = 3000.29d;
+double weightOfAPerson = 150.50d;
+double stockPrices = 123.4567d;
+double goldenRatio = 1.6180d;
+double speedOfLight = 4.7876d;
+
+System.out.println(bankBalance);
+System.out.println(weightOfAPerson);
+System.out.println(stockPrices);
+System.out.println(goldenRatio);
+System.out.println(speedOfLight);
+
+
+boolean areUFine= false;
+boolean isJavaEasy = true;
+boolean isTomorrowHoliday = false;
+boolean isModiSingle = false;
+boolean isIndiaDEvelopedCountry = false;
+boolean isTomorrowThursday = true;
+boolean isCodingEasy = true;
+boolean isIndiaDevelopingCountry = true;
+boolean animalsHaveEmotions = true;
+boolean sirTeachingIsGood = true;
+
+System.out.println(areUFine);
+System.out.println(isJavaEasy);
+System.out.println(isTomorrowHoliday);
+System.out.println(isModiSingle);
+System.out.println(isIndiaDEvelopedCountry);
+System.out.println(isTomorrowThursday);
+System.out.println(isCodingEasy);
+System.out.println(isIndiaDevelopingCountry);
+System.out.println(animalsHaveEmotions);
+System.out.println(sirTeachingIsGood);
+
+
+char gender = 'F';
+char section = 'A';
+char block = 'B';
+char grade = 'C';
+
+System.out.println(gender);
+System.out.println(section);
+System.out.println(block);
+System.out.println(grade);
+
+}
+
+}
