e98921d19295cdf29cc8240ec9a99d4f4b4dc6bb
keerthig12 <keerthimg02@gmail.com>
2024-10-18 11:50:10 +0530

    methods in java

---
+++ Method.java
@@ -0,0 +1,14 @@
+public class Method{
+
+     public static void main(String[] args) {
+
+          sendmessage();
+     }
+          
+          public static void sendmessage(){
+               System.out.println("ill send message");
+          
+     }
+}
+
+	
