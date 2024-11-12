6ec07920a21a488f8bf968c6132987a69075707b
keerthig12 <keerthimg02@gmail.com>
2024-11-13 00:56:36 +0530

    DataTypesUsingArray

---
+++ Array.java
@@ -0,0 +1,224 @@
+public class Array{
+
+    public static void main(String[] args) {
+	
+	   		int[] age = {12, 13, 14, 16, 18, 19, 21, 15, 22, 27};
+				System.out.println(age[0]);
+				System.out.println(age[1]);
+				System.out.println(age[2]);
+				System.out.println(age[3]);
+				System.out.println(age[4]);
+				System.out.println(age[5]);
+				System.out.println(age[6]);
+				System.out.println(age[7]);
+				System.out.println(age[8]);
+				System.out.println(age[9]);
+
+  			int[]ages = new int[10];
+ 				age [0]=23; 
+ 				age [1]=12;
+ 				age [2]=16;
+ 				age [3]=19;
+ 				age [4]=25;
+ 				age [5]=32;
+ 				age [6]=10;
+ 				age [7]=40;
+ 				age [8]=67;
+ 				age [9]=13;
+
+ 				System.out.println(age[0]);
+				System.out.println(age[1]);
+				System.out.println(age[2]);
+				System.out.println(age[3]);
+				System.out.println(age[4]);
+				System.out.println(age[5]);
+				System.out.println(age[6]);
+				System.out.println(age[7]);
+				System.out.println(age[8]);
+				System.out.println(age[9]);
+
+				byte[]numberOfPhonesInAHouse = {3,2,5,7,8,4,1,10,9,12};
+				System.out.println(numberOfPhonesInAHouse[0]);
+				System.out.println(numberOfPhonesInAHouse[1]);
+				System.out.println(numberOfPhonesInAHouse[2]);
+				System.out.println(numberOfPhonesInAHouse[3]);
+				System.out.println(numberOfPhonesInAHouse[4]);
+				System.out.println(numberOfPhonesInAHouse[5]);
+				System.out.println(numberOfPhonesInAHouse[6]);
+				System.out.println(numberOfPhonesInAHouse[7]);
+				System.out.println(numberOfPhonesInAHouse[8]);
+				System.out.println(numberOfPhonesInAHouse[9]);
+
+				byte[]numberOfPhonesInAHouseIs = new byte[10];
+				numberOfPhonesInAHouse[0]=11;
+				numberOfPhonesInAHouse[1]=22;
+				numberOfPhonesInAHouse[2]=33;
+				numberOfPhonesInAHouse[3]=44;
+				numberOfPhonesInAHouse[4]=55;
+				numberOfPhonesInAHouse[5]=66;
+				numberOfPhonesInAHouse[6]=77;
+				numberOfPhonesInAHouse[7]=88;
+				numberOfPhonesInAHouse[8]=99;
+				numberOfPhonesInAHouse[9]=13;
+
+				System.out.println(numberOfPhonesInAHouse[0]);
+				System.out.println(numberOfPhonesInAHouse[1]);
+				System.out.println(numberOfPhonesInAHouse[2]);
+				System.out.println(numberOfPhonesInAHouse[3]);
+				System.out.println(numberOfPhonesInAHouse[4]);
+				System.out.println(numberOfPhonesInAHouse[5]);
+				System.out.println(numberOfPhonesInAHouse[6]);
+				System.out.println(numberOfPhonesInAHouse[7]);
+				System.out.println(numberOfPhonesInAHouse[8]);
+				System.out.println(numberOfPhonesInAHouse[9]);
+
+				short[]damInIndia = {8647,8174,9034,9208,7589,8795,8975,8239,6584,1213};
+				System.out.println(damInIndia[0]);
+        System.out.println(damInIndia[1]);
+        System.out.println(damInIndia[2]);
+        System.out.println(damInIndia[3]);
+        System.out.println(damInIndia[4]);
+        System.out.println(damInIndia[5]);
+        System.out.println(damInIndia[6]);
+        System.out.println(damInIndia[7]);
+        System.out.println(damInIndia[8]);
+        System.out.println(damInIndia[9]);
+
+        short[]damsInIndia = new short[5];
+        damsInIndia[0]=22000;
+        damsInIndia[1]=13000;
+        damsInIndia[2]=14000;
+        damsInIndia[3]=15000;
+        damsInIndia[4]=16000;
+      
+        System.out.println(damInIndia[0]);
+        System.out.println(damInIndia[1]);
+        System.out.println(damInIndia[2]);
+        System.out.println(damInIndia[3]);
+        System.out.println(damInIndia[4]);
+
+        long[]creditCardNumbers = {543210987654321l, 93568366921558l,9852222279834l,7258356742971l, 7823655963392l, 637285757875l};
+        System.out.println(creditCardNumbers[0]);
+        System.out.println(creditCardNumbers[1]);
+        System.out.println(creditCardNumbers[2]);
+        System.out.println(creditCardNumbers[3]);
+        System.out.println(creditCardNumbers[4]);
+        System.out.println(creditCardNumbers[5]);
+
+        long[]creditCardNumber = new long[6];
+        creditCardNumbers[0]=12345678l;
+        creditCardNumbers[1]=12345678l;
+        creditCardNumbers[2]=12345678l;
+        creditCardNumbers[3]=12345678l;
+        creditCardNumbers[4]=12345678l;
+        creditCardNumbers[5]=12345678l;
+        
+        System.out.println(creditCardNumbers[0]);
+        System.out.println(creditCardNumbers[1]);
+        System.out.println(creditCardNumbers[2]);
+        System.out.println(creditCardNumbers[3]);
+        System.out.println(creditCardNumbers[4]);
+        System.out.println(creditCardNumbers[5]);
+
+        float[]oneUsd = {0.8765f, 0.1213f, 0.1323f, 0.1445f, 0.8555f,	0.322f, 2.1213f};
+        System.out.println(oneUsd[0]);
+        System.out.println(oneUsd[1]);
+				System.out.println(oneUsd[2]);
+				System.out.println(oneUsd[3]);
+				System.out.println(oneUsd[4]);
+				System.out.println(oneUsd[5]);
+
+				float[]oneUsdIs = new float[6];
+				oneUsdIs[1]=1.111f;
+				oneUsdIs[2]=2.222f;
+				oneUsdIs[3]=3.333f;
+				oneUsdIs[4]=4.444f;
+				oneUsdIs[5]=5.555f;
+        
+				System.out.println(oneUsdIs[1]);
+				System.out.println(oneUsdIs[2]);
+				System.out.println(oneUsdIs[3]);
+				System.out.println(oneUsdIs[4]);
+				System.out.println(oneUsdIs[5]);
+
+       double[]bankBalance = {3000.29d, 443.3274d, 3876.3744d, 8494.4789d, 6341.9374d, 8374.47d, 3467.549d, 980.098d, 543.67d, 289.89d, 728.48d};
+       System.out.println(bankBalance[0]);
+       System.out.println(bankBalance[1]);
+       System.out.println(bankBalance[2]);
+       System.out.println(bankBalance[3]);
+       System.out.println(bankBalance[4]);
+       System.out.println(bankBalance[5]);
+
+       double[]oneUsdIsOf = new double[11];
+       bankBalance[0]=111.11d;
+       bankBalance[1]=222.22d;
+       bankBalance[2]=333.33d;
+       bankBalance[3]=444.44d;
+       bankBalance[4]=555.55d;
+
+       System.out.println(bankBalance[0]);
+       System.out.println(bankBalance[1]);
+       System.out.println(bankBalance[2]);
+       System.out.println(bankBalance[3]);
+       System.out.println(bankBalance[4]);
+
+       boolean[]isJavaEasy = {true,true,false,false,true,false,true,false,true,false,true};
+       System.out.println(isJavaEasy[0]);
+       System.out.println(isJavaEasy[1]);
+       System.out.println(isJavaEasy[2]);
+       System.out.println(isJavaEasy[3]);
+       System.out.println(isJavaEasy[4]);
+       System.out.println(isJavaEasy[5]);
+       System.out.println(isJavaEasy[6]);
+       System.out.println(isJavaEasy[7]);
+       System.out.println(isJavaEasy[8]);
+       System.out.println(isJavaEasy[9]);
+
+       boolean[]isTheJavaEasy = new boolean[10];
+       isTheJavaEasy[0]=false;
+       isTheJavaEasy[1]=false;
+       isTheJavaEasy[2]=true;
+       isTheJavaEasy[3]=true;
+       isTheJavaEasy[4]=false;
+       isTheJavaEasy[5]=true;
+       isTheJavaEasy[6]=false;
+       isTheJavaEasy[7]=true;
+       isTheJavaEasy[8]=false;
+       isTheJavaEasy[9]=true;
+
+       System.out.println(isTheJavaEasy[0]);
+       System.out.println(isTheJavaEasy[1]);
+       System.out.println(isTheJavaEasy[2]);
+       System.out.println(isTheJavaEasy[3]);
+       System.out.println(isTheJavaEasy[4]);
+       System.out.println(isTheJavaEasy[5]);
+       System.out.println(isTheJavaEasy[6]);
+       System.out.println(isTheJavaEasy[7]);
+       System.out.println(isTheJavaEasy[8]);
+       System.out.println(isTheJavaEasy[9]);
+
+       char[]block = {'A','B','A','B','A','B','A','B','A','B'};
+       System.out.println(block[1]);
+       System.out.println(block[2]);
+       System.out.println(block[3]);
+       System.out.println(block[4]);
+       System.out.println(block[5]);
+       System.out.println(block[6]);
+       System.out.println(block[7]);
+
+       char[]blocks=new char[10];
+       blocks[2]='B';
+       blocks[4]='B';
+       blocks[6]='B';
+       blocks[8]='B';
+       blocks[1]='A';
+
+      System.out.println(blocks[2]);
+      System.out.println(blocks[4]);
+      System.out.println(blocks[6]);
+      System.out.println(blocks[8]);
+      System.out.println(blocks[1]);
+
+}
+
+}
